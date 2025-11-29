070d668b57f8aed09c31e53ded63e45db18feb92
viktorexe <developer.viktorexe@gmail.com>
2025-11-30 01:11:15 +0530

    modules

---
+++ 22. IO and Error Handling/06_modules.py
@@ -0,0 +1,8 @@
+# Modules are packages (or in simple word very ling codes sometimes written in dfferent languages)
+# which are useful in python for performing some operations like logging, math, date time 
+# We can make our own module 
+
+import math # importing a module
+from math import sqrt # Import specific function
+import math as m # Nickname 
+print(math.sqrt(100))
