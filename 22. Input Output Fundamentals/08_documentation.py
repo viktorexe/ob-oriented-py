cde9626fe6c57b8a1dac80af3178ee1237ce46b4
viktorexe <developer.viktorexe@gmail.com>
2025-11-30 01:20:18 +0530

    Directory renamed

---
+++ 22. Input Output Fundamentals/08_documentation.py
@@ -0,0 +1,9 @@
+# Documentation basically means
+# - Explaining Code 
+# - Adding meaningful explanations
+# Helpful for a reader / contributor or next developer
+
+# Types of documentation
+# - Comments
+# - Docstrings """This is a Docstring"""
+
