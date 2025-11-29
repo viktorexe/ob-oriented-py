dea6f448ee3b03a206871a2b6456451fc2e52ecb
viktorexe <developer.viktorexe@gmail.com>
2025-11-30 01:16:39 +0530

    packages

---
+++ 22. IO and Error Handling/07_packages.py
@@ -0,0 +1,13 @@
+# Packages are basically a folder of modules
+# We can make our own package, by making a folder in root directory inside an epty __init__.py and then utils file
+# Packages are used for organising code, otherwise everything in a single file makes it obfuscated 
+
+
+# my_package/
+#     __init__.py -> Empty file, initializes that folder as a package 
+#     calc.py
+#     utils.py
+
+
+import numpy # A package 
+import math 
